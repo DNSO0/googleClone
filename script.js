3895d4fbe1589123ecbf09a5f57982507e66727c
DNSO0 <toxicthrashd@gmail.com>
2024-09-30 21:21:06 -0500

    Proyecto clon de google

---
+++ script.js
@@ -0,0 +1,17 @@
+/*
+function contrasenaValida() {
+    let contrasena = ["2Fj(jjbFsuj", "eoZiugBf&g9"];
+    let valorUsuario = prompt("Ingresa tu contraseña:");
+  
+    if (contrasena[0] == valorUsuario) {
+      return true;
+    }if (contrasena[1] == valorUsuario){
+        return true;
+    }else {
+      return false;
+    }
+  
+}
+  let resultado = contrasenaValida();
+  console.log(resultado);
+*/
